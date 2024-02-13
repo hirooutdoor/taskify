57f513ede658526a4270ce92670065f68a6cc6d3
hirooutdoor <hmaeyama0510@gmail.com>
2024-02-14 06:36:46 +0900

    add common layout page

---
+++ src/app/(common)/NotFound/page.tsx
@@ -0,0 +1,8 @@
+const NotFound = () => (
+  <div className='flex flex-col gap-8 justify-center text-center h-full text-3xl text-gray-500'>
+    <div>404</div>
+    <h1>お探しのページが見つかりません。</h1>
+  </div>
+);
+
+export default NotFound;
