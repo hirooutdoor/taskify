2e8d36b5f811cdad14d4c6da7f3cc86adfae20ab
hirooutdoor <hmaeyama0510@gmail.com>
2024-02-28 07:52:30 +0900

    feat: config middleware for redirection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,29 @@
-import { authMiddleware } from '@clerk/nextjs';
+import { authMiddleware, redirectToSignIn } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
 
 export default authMiddleware({
-  publicRoutes: ['/']
+  publicRoutes: ['/'],
+  afterAuth(auth, req) {
+    if (auth.userId && auth.isPublicRoute) {
+      let path = '/organization';
+
+      if (auth.orgId) {
+        path += `/${auth.orgId}`;
+      }
+
+      const orgSelectionUrl = new URL(path, req.url);
+      return NextResponse.redirect(orgSelectionUrl);
+    }
+
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== '/select-org') {
+      const orgSelectionUrl = new URL('/select-org', req.url);
+      return NextResponse.redirect(orgSelectionUrl);
+    }
+  }
 });
 
 export const config = {
